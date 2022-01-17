eee1428e5ef1cbcea1472d4b0e8d10f40f1a5968
m-ata <muhammad_ata@outlook.com>
2022-01-17 16:46:28 +0300

    feat: added translations for account page

--- pages/account.tsx
+++ pages/account.tsx
@@ -1,12 +1,13 @@
-import { ExclamationIcon } from '@heroicons/react/solid';
-import { Subscription } from '@prisma/client';
-import type { GetServerSideProps, NextPage } from 'next';
-import Link from 'next/link';
-import { getSession } from 'next-auth/react';
-import React from 'react';
+import { ExclamationIcon } from "@heroicons/react/solid";
+import { Subscription } from "@prisma/client";
+import type { GetServerSideProps, NextPage } from "next";
+import Link from "next/link";
+import { getSession } from "next-auth/react";
+import React from "react";
 
-import { Navbar } from '@app/components';
-import { prisma } from '@app/utils/ssr';
+import { Navbar } from "@app/components";
+import { prisma } from "@app/utils/ssr";
+import { LocalizedText } from "../components/LocalizeText";
 
 type Props = {
   subscription: Subscription | null;
@@ -14,10 +15,10 @@ type Props = {
 
 const Page: NextPage<Props> = ({ subscription }) => {
   const handleCreatePortal = async () => {
-    const res = await fetch('/api/stripe/create-portal', {
-      method: 'POST',
+    const res = await fetch("/api/stripe/create-portal", {
+      method: "POST",
       headers: {
-        'content-type': 'application/json',
+        "content-type": "application/json",
       },
     });
 
@@ -35,16 +36,16 @@ const Page: NextPage<Props> = ({ subscription }) => {
       <div className="px-4 py-24 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="max-w-3xl mx-auto">
           <h1 className="text-6xl font-extrabold text-gray-800 sm:text-center">
-            Account
+            <LocalizedText t={"ACCOUNT.ACCOUNT"} />
           </h1>
           <p className="mt-5 text-xl text-gray-500 sm:text-center">
-            We partnered with Stripe for a simplified billing.
+            <LocalizedText t={"ACCOUNT.STRIPE_BILLING"} />
           </p>
 
           <div className="mt-12 bg-white shadow sm:rounded-lg">
             <div className="px-4 py-5 sm:p-6">
               <h3 className="text-lg font-medium leading-6 text-gray-900">
-                Manage subscription
+                <LocalizedText t={"ACCOUNT.MANAGE_SUBSCRIPTION"} />
               </h3>
               {!!subscription?.cancel_at && (
                 <div className="p-4 my-6 border-l-4 border-yellow-400 bg-yellow-50">
@@ -57,7 +58,7 @@ const Page: NextPage<Props> = ({ subscription }) => {
                     </div>
                     <div className="ml-3">
                       <p className="text-sm text-yellow-700">
-                        Your subscription will end the{' '}
+                        <LocalizedText t={"ACCOUNT.SUBSCRIPTION_END"} />{" "}
                         {subscription.cancel_at.toLocaleDateString()}
                       </p>
                     </div>
@@ -67,8 +68,7 @@ const Page: NextPage<Props> = ({ subscription }) => {
               <div className="mt-2 sm:flex sm:items-start sm:justify-between">
                 <div className="max-w-xl text-sm text-gray-500">
                   <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Recusandae voluptatibus corrupti atque repudiandae nam.
+                    <LocalizedText t={"ACCOUNT.LOREM_IPSUM"} />
                   </p>
                 </div>
                 <div className="mt-5 sm:mt-0 sm:ml-6 sm:flex-shrink-0 sm:flex sm:items-center">
@@ -78,7 +78,7 @@ const Page: NextPage<Props> = ({ subscription }) => {
                       type="button"
                       className="inline-flex items-center px-4 py-2 font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                     >
-                      Change plan
+                      <LocalizedText t={"ACCOUNT.CHANGE_PLAN"} />
                     </button>
                   ) : (
                     <Link href="/">
@@ -86,7 +86,7 @@ const Page: NextPage<Props> = ({ subscription }) => {
                         type="button"
                         className="inline-flex items-center px-4 py-2 font-medium text-white bg-indigo-600 border border-transparent rounded-md shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
                       >
-                        Subscribe
+                        <LocalizedText t={"ACCOUNT.SUBSCRIBE"} />
                       </button>
                     </Link>
                   )}
@@ -108,7 +108,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
     where: {
       userId: (session?.user as any)?.userId,
       status: {
-        in: ['active', 'trialing'],
+        in: ["active", "trialing"],
       },
     },
     include: {
@@ -122,7 +122,7 @@ export const getServerSideProps: GetServerSideProps<Props> = async (
 
   return {
     redirect: !session && {
-      destination: '/api/auth/signin',
+      destination: "/api/auth/signin",
       permanent: false,
     },
     props: {
